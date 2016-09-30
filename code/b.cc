9a7c56f07e8cd5b52a27f905ceb6d4165dcfcb0b
Fredrik Østrem <frx.apps@gmail.com>
2016-09-30 17:25:28 +0200

    feet

--- code/b.cc
+++ code/b.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <armadillo>
 #include <ctime>
 #include <cassert>
@@ -8,11 +9,8 @@ using namespace std;
 using namespace arma;
 
 const double rmin = 0;
-const double rmax = 10;
-size_t N = 41;
+const double rmax = 6;
 const double epsilon = 1e-10;
-
-const double h = (rmax - rmin) / (N - 1);
 
 // potential function
 static constexpr double V(double r) {
@@ -72,26 +70,28 @@ static double comp_eig(vec Avalues, mat Avectors, vec Bvalues, mat Bvectors) {
     Bvectors.swap_cols(i, k);
   }
 
-  Bvalues.print();
-
   // fix antiparallell eigenvectors in A and B
   for(size_t i = 0; i < M; i++) {
-    if(Avectors(0, i) * Bvectors(0, i) < 0)
+    if(arma::dot(Avectors.col(i), Bvectors.col(i)) < 0)
       Bvectors.col(i) = - Bvectors.col(i);
   }
 
-  // now that A and B have been sorted (so eigenvectors and eigenvalues should be in the same order)
-  // calculate an error metric based on the dimensionality M
-  const double err = norm(Avalues - Bvalues) / sqrt(M) + norm(Avectors - Bvectors) / M;
+  // fix maximal difference between eigenvalues or eigenvectors
+  const double values_error = arma::abs(Avalues - Bvalues).max();
+  const double vectors_error = arma::abs(Avectors - Bvectors).max();
 
   // return error metric
-  return err;
+  return std::max(values_error, vectors_error);
 }
 
 // solve with Jacobi's method
 // A is input matrix, P is eigenvector matrix (with column vector eigenvectors), L is eigenvalue vector
-static void jacobi_solve(const mat &A, mat &P, vec &L) {
-  // assume A is N x N
+static void jacobi_solve(const mat &A, mat &P, vec &L, size_t &steps, double &step_time) {
+  // get N from A matrix
+  const size_t N = A.n_rows;
+  assert(A.n_cols == N);
+
+  std::cout << "N=" << N << std::endl;
 
   // B is similar matrix to A through matrix transforms
   mat B = A;
@@ -103,7 +103,7 @@ static void jacobi_solve(const mat &A, mat &P, vec &L) {
 
   clock_t time_start, time_end;
   double time_total = 0;
-  size_t step = 0;
+  steps = 0;
   while(true) {
     // timing
     time_start = clock();
@@ -138,10 +138,13 @@ static void jacobi_solve(const mat &A, mat &P, vec &L) {
     // timing
     time_end = clock();
     time_total += (double)(time_end - time_start) / CLOCKS_PER_SEC;
-    step++;
+    steps++;
   }
 
-  std::cout << "JACOBI DONE (N = " << N << ", steps = " << step << ", time per step = " << (1000 * time_total / step) << "ms)" << std::endl;
+  // export steps and time per step
+  step_time = time_total / steps;
+
+  std::cout << "JACOBI DONE (N = " << N << ", steps = " << steps << ", time per step = " << (1000 * step_time) << "ms)" << std::endl;
 
   // find eigenvalues
   L.set_size(N);
@@ -149,7 +152,9 @@ static void jacobi_solve(const mat &A, mat &P, vec &L) {
     L(i) = B(i, i);
 }
 
-int main(int argc, char **argv) {
+int run_program(size_t N, size_t &steps, double &step_time, double &err) {
+  const double h = (rmax - rmin) / N;
+
   // array of ρ valuses
   vec r(N);
   for(size_t i = 0; i < N; i++)
@@ -164,7 +169,7 @@ int main(int argc, char **argv) {
     d(i) = 2 / (h * h) + V(r[i]);
 
   // calculate matrix A
-  mat A(N, N);
+  mat A(N, N, arma::fill::zeros);
   for(size_t i = 0; i < N; i++) {
     A(i, i) = d(i);
 
@@ -177,13 +182,38 @@ int main(int argc, char **argv) {
   // solve with Jacobi method
   vec eigenvalues;
   mat eigenvectors;
-  jacobi_solve(A, eigenvectors, eigenvalues);
+  jacobi_solve(A, eigenvectors, eigenvalues, steps, step_time);
 
   // solve with Armadillo's eigsys
   vec arma_eigenvalues;
   mat arma_eigenvectors;
   eig_sym(arma_eigenvalues, arma_eigenvectors, A);
 
-  double err = comp_eig(eigenvalues, eigenvectors, arma_eigenvalues, arma_eigenvectors);
-  std::cout << err << std::endl;
+  err = comp_eig(eigenvalues, eigenvectors, arma_eigenvalues, arma_eigenvectors);
+}
+
+int main(int argc, char **argv) {
+  const size_t Nvalues[] = { 4, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+  const size_t Nlen = sizeof(Nvalues) / sizeof(*Nvalues);
+
+  FILE *fp = fopen("b_steps.dat", "w");
+  fprintf(fp, "N          epsilon    steps      step_time  error\n");
+
+  for(size_t i = 0; i < Nlen; i++) {
+    size_t N = i[Nvalues]; // aww yeah abusing valid C++ syntax for no reason
+
+    size_t K; // number of steps
+    double t; // time per step
+    double e; // average error
+
+    std::cout << "running with N = " << N << std::endl;
+
+    run_program(N, K, t, e);
+
+    // write result row
+    fprintf(fp, "%-10ld %-10.5g %-10ld %-10.5g %-10.5g\n", N, epsilon, K, t, e);
+    fflush(fp);
+  }
+
+  fclose(fp);
 }
